5e8951665b01023744ead69bf1d99a7f94a8cc65
sayanandh <sayanandhaneesh@gmail.com>
2025-06-05 10:29:56 +0530

    Fundamen_1

---
+++ Basic_Fundamentals/index.js
@@ -0,0 +1,12 @@
+// const item ={
+//     "title":"The Movieee",
+//     "description":"The Movieee's name",
+//     "author":"Mary",
+//     "published":true,
+//     "rating":true,
+//     "year":1,
+//     "year2":2,
+//     "year3":3,
+//     "year4":4,
+// }
+// console.log(item)

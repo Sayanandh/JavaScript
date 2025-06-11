eee3d0ae3d7a9a266cbf2d2c47f006663034da5e
sayanandh <sayanandhaneesh@gmail.com>
2025-06-12 00:21:46 +0530

    day 2 final

--- Basic_Fundamentals/index.js
+++ Basic_Fundamentals/index.js
@@ -1,6 +1,6 @@
 // const item ={
 //     "title":"The Movieee",
-//     "description":"The Movieee's name",
+//     "description":"The Movieee's na",
 //     "author":"Mary",
 //     "published":true,
 //     "rating":true,
@@ -24,3 +24,43 @@
 // false
 // let c = prompt("nigga whast ru age ")
 // console.log(typeof(a))
+// let age= console.log("enter baba")
+// console.log(age)
+// const sum = (p,q) => {
+//     p=p*10
+//     q=q*10
+//     return p+q
+// }
+// console.log(sum(11,12))
+// let marks={
+//     harry:"90",
+//     rinky:"95"
+// }
+// for ( let key in marks){
+//     console.log(key)
+// }
+
+// const mean = (a,b,c,d)=> {
+//     return Math.round((a +b+c+d)/4)
+// }
+// console.log(mean(12,23,34,45))
+
+// ****************************//
+// ****************************//
+//            Strings          //
+// ****************************//
+// ****************************//
+// let na = "sayu"
+// console.log(na.length)
+// console.log(na[0])
+// console.log(na[1])
+// console.log(na[2])
+// console.log(na[3])
+
+// tempate literal
+// let boy1 = "sayu\n"
+// let boy2 = "niggu"
+// let sentace = `${boy1} is a friend of ${boy2}`
+// console.log(sentace)
+// console.log(boy1.length)
+// console.log(boy1.toUpperCase())
---
+++ browser/script.js
@@ -0,0 +1,20 @@
+
+// let c = prompt("giv age")
+// c = Number.parseInt(c)
+// const candrive= (c) =>{
+//     if (c<18){
+//         return false
+//     }
+//     else{
+//         return true
+//     }
+// }
+// if (candrive(c)){
+//     alert("go drive baba")
+// }
+// else {
+//     alert("u nono drive ")
+// }
+
+// let colour = prompt("give clr ")
+// document.body.style.background=colour
